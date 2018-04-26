6e0e7805fb0a720eb2532fd2dbd56836817335a3
Saman Barghi <saman.b@gmail.com>
2018-04-26 09:44:43 -0400

    pthreads: fixed arguments

--- servers/pthreads/main.cpp
+++ servers/pthreads/main.cpp
@@ -13,7 +13,7 @@ const HTTPResponse helloWorld(const HTTPRequest& request){
 
 int main(int argc, char *argv[]) {
 
-    if (argc != 2) {
+    if (argc < 2) {
         printf("USAGE: %s NUMBER_OF_CORES\n", argv[0]);
         return 0;
     }
